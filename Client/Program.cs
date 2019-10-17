2655c10becb0531e56eae02a886519358b4570fa
siinch <simon.ingemanns@protonmail.com>
2019-10-17 19:38:25 +0200

    helpfunctions

---
+++ Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var client = new TcpClient();
+                client.Connect(IPAddress.Loopback, 5000);
+
+                var stream = client.GetStream();
+
+
+                Console.WriteLine("Send message:");
+                var msg = Console.ReadLine();
+
+                Console.WriteLine($"Message: {msg}");
+                var buffer = Encoding.UTF8.GetBytes(msg);
+
+                stream.Write(buffer, 0, buffer.Length);
+
+                if (msg == "exit") break;
+
+                buffer = new byte[client.ReceiveBufferSize];
+                var rcnt = stream.Read(buffer, 0, buffer.Length);
+
+                msg = Encoding.UTF8.GetString(buffer, 0, rcnt);
+
+                Console.WriteLine($"Message: {msg}");
+
+                stream.Close();
+            }
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Server
 {
+    public class Request
+    {
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string Date { get; set; }
+        public string Body { get; set; }
+    }
+    
+    public class Category
+    {
+        [JsonPropertyName("cid")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //setting up server
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 5000;
+            var server = new TcpListener(ip, port);
+            server.Start();
+            
+            while (true)
+            {
+                //receive message from client
+                Console.WriteLine("Waiting for client...");
+                var client = server.AcceptTcpClient(); ;
+                var stream = client.GetStream();
+                var buffer = new byte[client.ReceiveBufferSize];
+                var rcnt = stream.Read(buffer, 0, buffer.Length);
+                var message = Encoding.UTF8.GetString(buffer, 0, rcnt);
+
+                var response = new {Response = ""};
+                if (message == "exit") break;
+                if (message == "{}") response = new {Response = "Missing method"};
+                
+                Console.WriteLine();
+                
+                //sending response to client
+                //Console.WriteLine($"Message: {message}");
+                //message = message.ToUpper();
+                buffer = Encoding.UTF8.GetBytes(response.ToJson());
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Close();
+            }
+
+            server.Stop();
+        }
+    }
+    
+    public static class Util
+    {
+        public static string ToJson(this object data)
+        {
+            return JsonSerializer.Serialize(data, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        }
+
+        public static T FromJson<T>(this string element)
+        {
+            return JsonSerializer.Deserialize<T>(element, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        }
+
+        public static void SendResponse(this TcpClient client, string response)
+        {
+            var msg = Encoding.UTF8.GetBytes(response);
+            client.GetStream().Write(msg, 0, msg.Length);
+        }
+
+        public static Request ReadRequest(this TcpClient client)
+        {
+            var strm = client.GetStream();
+            //strm.ReadTimeout = 250;
+            byte[] request = new byte[2048];
+            using (var memStream = new MemoryStream())
+            {
+                int bytesread = 0;
+                do
+                {
+                    bytesread = strm.Read(request, 0, request.Length);
+                    memStream.Write(request, 0, bytesread);
+
+                } while (bytesread == 2048);
+                
+                var requestData = Encoding.UTF8.GetString(memStream.ToArray());
+                return JsonSerializer.Deserialize<Request>(requestData, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+            }
         }
     }
 }
